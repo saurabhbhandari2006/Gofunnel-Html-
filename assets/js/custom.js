b250b9fee1d4ba1b5e038b42d3341bbdc9d954c1
agarwalmehul <agarwalmehul92@gmail.com>
2014-10-16 17:21:49 +0530

    added js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,29 +1,93 @@
-$(document).ready(function () {
 
-    initDataOnClick();
+var answer = [];
+var result = [];
+var aed;
 
-});
+var gameSelected = false;
 
-function initDataOnClick() {
-    for (var i = 1; i <= 4; i++) {
-        var targetDiv = document.getElementById(i);
-        targetDiv.onclick = gameData(i);
-    }
+function initTheme() {
+
 }
-function gameData(id) {
 
-    $("#" + id).click(function () {
-        $("#game-select").hide();
-        $("#game-play-div").show();
-        for (var i = 1; i <= 4; i++) {
-            console.log(i);
-            $("#game-box-" + i).hide();
+function initGame() {
+    aed = initAED;
+}
+
+
+
+
+function gameSelect() {
+    $("#gamePlay").fadeOut();
+    $("#gameSelect").fadeIn();
+    $(".games").on('click', function() {
+        if(gameSelected == false) {
+            gameSelected = true;
+            switch(this.attr("id")) {
+                case "1":
+                    showGame(1);
+                    game1(this.attr("id"));
+                    break;
+
+                case "2":
+                    showGame(2);
+                    game2(this.attr("id"));
+                    break;
+
+                case "3":
+                    showGame(3);
+                    game3(this.attr("id"));
+                    break;
+
+                case "4":
+                    showGame(4);
+                    game4(this.attr("id"));
+                    break;
+            }
         }
-        $("#game-box-" + id).show();
     });
+}
+
+function showGame(id) {
+    $("#gameSelect").fadeOut();
+    $("#gamePlay").fadeIn();
+    $("#game1").fadeOut();
+    $("#game2").fadeOut();
+    $("#game3").fadeOut();
+    $("#game4").fadeOut();
+
+    var div = "#game"+id;
+    $(div).fadeIn();
 
 }
-function back(){
-    $("#game-select").show();
-    $("#game-play-div").hide();
+
+function game1(id) {
+    getQuestions();
+
+
+}
+
+function game2(id) {
+
+}
+
+function game3(id) {
+
+}
+
+function game4(id) {
+
+}
+
+
+
+function getQuestions() {
+
+}
+
+function showQuestions() {
+
+}
+
+function processAnswer() {
+
 }
