b4d0ad3d354e798475c2256d46975d724b973684
saurabhbhandari2006 <saurabhbhandari2006@gmail.com>
2014-10-16 15:39:43 +0530

    first git entry

---
+++ assets/js/custom-data.js
@@ -0,0 +1,3 @@
+/**
+ * Created by saurabh on 16/10/14.
+ */
---
+++ assets/js/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    initDataOnClick();
+
+});
+
+function initDataOnClick() {
+    for (var i = 1; i <= 4; i++) {
+        var targetDiv = document.getElementById(i);
+        targetDiv.onclick = gameData(i);
+    }
+}
+function gameData(id) {
+
+    $("#" + id).click(function () {
+        $("#game-select").hide();
+        $("#game-play-div").show();
+        for (var i = 1; i <= 4; i++) {
+            console.log(i);
+            $("#game-box-" + i).hide();
+        }
+        $("#game-box-" + id).show();
+    });
+
+}
+function back(){
+    $("#game-select").show();
+    $("#game-play-div").hide();
+}
