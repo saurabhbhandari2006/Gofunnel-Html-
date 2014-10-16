e967e45312f62d769b458b72953271321930d2f0
agarwalmehul <agarwalmehul92@gmail.com>
2014-10-16 19:36:13 +0530

    logicaly working

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,13 @@
 
-var answer = [];
-var result = [];
+var answer;
+var result;
 var aed;
 
 var gameSelected = false;
+var answerSelected = false;
+var newQuestion;
+
+var questionList;
 
 function initTheme() {
 
@@ -11,83 +15,114 @@ function initTheme() {
 
 function initGame() {
     aed = initAED;
+    gameSelect();
 }
 
 
 
 
 function gameSelect() {
-    $("#gamePlay").fadeOut();
+    answer = [];
+    result = [];
+    $("#game").fadeOut();
+    $("#message").fadeOut();
     $("#gameSelect").fadeIn();
+    $("#endResult").fadeOut();
+    $("#qid").text(0);
     $(".games").on('click', function() {
         if(gameSelected == false) {
             gameSelected = true;
-            switch(this.attr("id")) {
-                case "1":
-                    showGame(1);
-                    game1(this.attr("id"));
-                    break;
-
-                case "2":
-                    showGame(2);
-                    game2(this.attr("id"));
-                    break;
-
-                case "3":
-                    showGame(3);
-                    game3(this.attr("id"));
-                    break;
-
-                case "4":
-                    showGame(4);
-                    game4(this.attr("id"));
-                    break;
-            }
+            var id = $(this).attr("id");
+            var mode = ""+id.charAt(id.length-1);
+            setQuestions(mode);
+            showGame();
+            setTimeout(function() {
+                startGame(mode);
+            }, 2000);
         }
     });
 }
 
-function showGame(id) {
+function setQuestions(mode) {
+    questionList = $.grep(questions, function(element) {
+        return element.mode_master == mode;
+    });
+    questionList = shuffle(questionList);
+}
+
+function showGame() {
     $("#gameSelect").fadeOut();
-    $("#gamePlay").fadeIn();
-    $("#game1").fadeOut();
-    $("#game2").fadeOut();
-    $("#game3").fadeOut();
-    $("#game4").fadeOut();
+    $("#game").fadeIn();
+    newQuestion = true;
+}
 
-    var div = "#game"+id;
-    $(div).fadeIn();
+function showQuestion(mode) {
+    var qid = parseInt($("#qid").text());
+    var question = questionList[qid];
+    console.log(question);
+    $("#question-statement").text(question.statement);
+    $("#opta").text(question.opta);
+    $("#optb").text(question.optb);
+    $("#optc").text(question.optc);
+    $("#optd").text(question.optd);
+
+    bindAnswer(mode);
+}
+
+function startGame(mode) {
+    var qid = parseInt($("#qid").text());
+    if(qid < 5) {
+        showQuestion(mode);
+    } else {
+        showEndResult();
+    }
 
 }
 
-function game1(id) {
-    getQuestions();
-
-
+function bindAnswer(mode) {
+    $(".answer").on('click', function() {
+        if(answerSelected == false) {
+            answerSelected = true;
+            answer.push($(this).attr("id"));
+            processAnswer($(this).attr("id"), mode);
+        }
+    });
 }
 
-function game2(id) {
+function processAnswer(answer, mode) {
+    var ans = ""+answer.charAt(answer.length-1)
+    var qid = parseInt($("#qid").text());
+    if(questionList[qid].correct == ans) {
+        result.push("correct");
+    } else {
+        if(mode == 3 || mode == 4)
+        {
+            $("#message").fadeIn();
+            $("#message").text("Uh-oh! You lost the client.");
+            setTimeout(function() {
+                gameSelect();
+            }, 2000)
+        } else {
+            result.push("wrong");
+        }
 
+    }
+    $("#result").text(result[qid]);
+    qid++;
+    $("#qid").text(qid);
+    setTimeout(function() {
+        answerSelected = false;
+        $("#result").text("");
+        startGame(mode);
+    }, 2000);
 }
 
-function game3(id) {
-
-}
-
-function game4(id) {
-
-}
-
-
-
-function getQuestions() {
-
-}
-
-function showQuestions() {
-
-}
-
-function processAnswer() {
-
+function showEndResult() {
+    $("#game").fadeOut();
+    $("#endResult").fadeIn();
+    setTimeout(function() {
+        for(var i=0; i<5; i++){
+            $("#endResult").append("<tr><td align='left'> Question " + (i+1) + "</td><td align='right'>"+ result[i] + "</td></tr>");
+        }
+    }, 1000);
 }
