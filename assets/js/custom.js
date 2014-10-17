c8c70d4c8c801bbb0b3b47612377a74d945aa804
saurabhbhandari2006 <saurabhbhandari2006@gmail.com>
2014-10-17 12:11:49 +0530

    question page done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,29 +1,5 @@
-$(document).ready(function () {
+function startQuestion () {
 
-    initDataOnClick();
-
-});
-
-function initDataOnClick() {
-    for (var i = 1; i <= 4; i++) {
-        var targetDiv = document.getElementById(i);
-        targetDiv.onclick = gameData(i);
-    }
-}
-function gameData(id) {
-
-    $("#" + id).click(function () {
-        $("#game-select").hide();
-        $("#game-play-div").show();
-        for (var i = 1; i <= 4; i++) {
-            console.log(i);
-            $("#game-box-" + i).hide();
-        }
-        $("#game-box-" + id).show();
-    });
-
-}
-function back(){
-    $("#game-select").show();
-    $("#game-play-div").hide();
-}
+    $("#start-page").hide();
+    $("#question-page").show();
+};
